28415c3d304d4cb9a1622298a3b91b017c8b560a
Minjik06 <minjik525@deltalearns.ca>
2023-09-27 13:32:02 -0700

    ad

--- task5.py
+++ task5.py
@@ -36,3 +36,33 @@ You have:
  3 wood
  >
 """
+item=["food", "water","rope", "torch", "sack", "wood", "iron","steel", "ginger", "garlic", "fish", "stone", "wool"]
+itemN=[]
+for i in item:
+    itemN.append(0)
+
+item1={}
+item1=dict(zip(item,itemN))
+
+a=str(input("get or remove or finish : "))
+while a!= "finish":
+    if a=="get":
+        b=str(input("what item : "))
+        c=int(input("amount of items : "))
+        item1[b]+=c
+        a=str(input("get or remove or finish : "))
+    elif a=="remove":
+        b=str(input("what item : "))
+        print("\n")
+        print(f"you have ({b} : {item1[b]}) now")
+        print("\n")
+        c=int(input("amount of items : "))
+        item1[b]-=c
+        a=str(input("get or remove or finish : "))
+print(item1)
+
+"""while item1[b]<c:
+            print(f"you cannot remove {c} of {b} item")
+            c=int(input("re-enter amount of items or put 0 cancel the removing item: "))
+            if c==0:
+                break"""
