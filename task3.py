5eb81833be76000a60f4fd38f431105045fbf473
Minjik06 <minjik525@deltalearns.ca>
2023-09-27 15:49:46 +0900

    nk

--- task3.py
+++ task3.py
@@ -9,6 +9,9 @@ Write a python script display the values of the dictionary
 """
 sortMe = {1: -2,   2: 6,   4: 0,   6: 1,   9: 2,   10: 3,  11: 0,  13: 3,  14: 4,   15: -2,  17: 0,   18: -1,   20: 3}
 sort=[]
+
+
+
 a=1
 l=sor
 for j in sortMe:
